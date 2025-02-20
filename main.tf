b6a0549112609e9bce44ebf654640101d43cce7a
Edgar Mocharnyk <e.mocharnyk@scalr.com>
2025-02-20 17:37:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 terraform {
     required_providers {
         scalr = {
-            source = "registry.scalr.dev/scalr/scalr"
-            version= "1.0.0-rc-develop"
+            source = "scalr/scalr"
         }
     }
 }
